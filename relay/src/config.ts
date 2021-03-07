14e9ef7ccc72747a5e001685839c954f69667cc7
Constantin Schomburg <me@cschomburg.com>
2021-03-07 12:58:45 +0100

    Allow different config path, so you can use your own.

--- relay/src/config.ts
+++ relay/src/config.ts
@@ -5,7 +5,14 @@ export interface Config {
 }
 
 export async function loadConfig(): Promise<Config> {
-  const text = await Deno.readTextFile('config.json');
+  let path = '';
+  try {
+    path = Deno.env.get('HTTN_RELAY_CONFIG') || '';
+  } catch { }
+  path ||= 'config.json';
+
+  console.log('Loading config from', path);
+  const text = await Deno.readTextFile(path);
   const config = JSON.parse(text) as Config;
 
   return config;
